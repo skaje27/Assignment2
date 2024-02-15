5004abbaff50487867bdf271f22d26b20129c7b0
Shreya <128571799+shreyakulal55@users.noreply.github.com>
2024-02-15 23:52:03 +0530

    Update app.js

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const builtinMiddleware = require('./middleware/builtinMiddleware');
 const applicationMiddleware = require('./middleware/applicationMiddleware');
 const configurableMiddleware = require('./middleware/configurableMiddleware');
-const routerMiddleware=require('./middleware/routerMiddleware');
+const routerMiddleware = require('./middleware/routerMiddleware');
 
 const authRoutes = require('./routes/authRoutes');
 const blogRoutes = require('./routes/blogRoutes');
@@ -12,10 +12,8 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(applicationMiddleware.commonMiddleware);
-
 app.use(builtinMiddleware);
 app.use(routerMiddleware);
-app.use(applicationMiddleware);
 app.use(configurableMiddleware({ param1: 'value1', param2: 'value2' }));
 
 
@@ -25,4 +23,3 @@ app.use('/blogs', blogRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
